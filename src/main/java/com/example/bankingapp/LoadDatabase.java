004c7a12f88850eb0d43acbe7756ffa5aa8dfd80
Michael.van.der.Bend <michael.van.der.bend@rabobank.com>
2024-02-28 13:51:51 +0100

    * added automatic test user in DB

--- src/main/java/com/example/bankingapp/LoadDatabase.java
+++ src/main/java/com/example/bankingapp/LoadDatabase.java
@@ -5,8 +5,7 @@ import com.example.bankingapp.model.User;
 import com.example.bankingapp.repository.TransactionRepository;
 import com.example.bankingapp.repository.UserRepository;
 import com.example.bankingapp.util.JSONUtil;
-import org.json.simple.JSONObject;
-import org.json.simple.parser.ParseException;
+import org.json.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,32 +14,52 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
-import java.io.*;
-import java.math.BigDecimal;
-import java.util.ArrayList;
-
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Configuration
 class LoadDatabase {
 
     private static final Logger log = LoggerFactory.getLogger(LoadDatabase.class);
+
     @Autowired
     private PasswordEncoder passwordEncoder;
 
     @Bean
-    CommandLineRunner initDatabase(TransactionRepository repository, UserRepository userRepository) throws IOException, ParseException {
-        FileReader file = new FileReader("C://YEPP/bankingapp-backend/src/main/java/com/example/bankingapp/resources/DUMMY_TRANSACTIONS.json");
-        FileReader fileUsers = new FileReader("C://YEPP/bankingapp-backend/src/main/java/com/example/bankingapp/resources/DUMMY_USERS.json");
-        ArrayList<JSONObject> jsonTransactions = JSONUtil.fromJSONToArrayList(file);
-        ArrayList<JSONObject> jsonUsers = JSONUtil.fromJSONToArrayList(fileUsers);
-//        userRepository.save(new User(jsonUsers.get(0)));
-
+    CommandLineRunner initDatabase(TransactionRepository repository, UserRepository userRepository) {
         return args -> {
-            jsonTransactions.forEach(transaction -> log.info("Preloading: " + repository.save(new Transaction(transaction))));
-            jsonUsers.forEach(user -> log.info("Preloading: " + userRepository.save(User.builder()
-                    .userName("test").password(passwordEncoder.encode("test")).build())));
+            try {
+                List<Transaction> transactions = readTransactionsFromFile("C://YEPP/bankingapp-backend/src/main/java/com/example/bankingapp/resources/DUMMY_TRANSACTIONS.json");
+                List<User> users = readUsersFromFile("C://YEPP/bankingapp-backend/src/main/java/com/example/bankingapp/resources/DUMMY_USERS.json");
+
+                transactions.forEach(transaction -> log.info("Preloading transaction: " + repository.save(transaction)));
+                users.forEach(user -> log.info("Preloading user: " + userRepository.save(user)));
+            } catch (IOException e) {
+                log.error("Error reading data from files: " + e.getMessage());
+            }
         };
     }
 
+    private List<Transaction> readTransactionsFromFile(String filePath) throws IOException {
+        return Files.lines(Path.of(filePath))
+                .map(JSONUtil::fromJSONToTransaction)
+                .collect(Collectors.toList());
+    }
 
+    private List<User> readUsersFromFile(String filePath) throws IOException {
+        return Files.lines(Path.of(filePath))
+                .map(JSONUtil::fromJSONToUser)
+                .collect(Collectors.toList());
+    }
+
+    private User fromJSONToUser(JSONObject jsonObject) {
+        String userName = jsonObject.get("userName").toString();
+        String password = jsonObject.get("password").toString();
+        return User.builder().userName("test")
+                .password(passwordEncoder.encode("test"))
+                .build();
+    }
 }
